b56b9d9dddec057c9c51e40acf6c9048ebc2fc2d
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-16 19:52:08 +0100

    frontend NavBar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div><h2>NavBar</h2></div>
+  )
+}
+
+export default NavBar
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+content: [
+  './src/pages/**/*.{html,js}',
+  './src/components/**/*.{html,js}',
+],
   theme: {
     extend: {},
   },
