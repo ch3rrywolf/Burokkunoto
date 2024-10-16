c046d7c98405d0a04f258946146a9de74e3d3d30
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-16 23:08:24 +0100

    frontend Login form . handle,errorMsg,valdiateForm

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,14 +1,36 @@
 import React, { useState } from 'react'
 import PasswordInput from '../../components/Input/PasswordInput'
+import { Link } from 'react-router-dom'
+import { validateEmail } from '../../utils/helper'
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address")
+      return
+    }
+
+    if(!password) {
+      setError("Please enter the password")
+      return
+    }
+
+    setError("")
+
+    // Login Api :>
+  }
+
+
   return (
-    <div className="">
-      <div>
-        <form>
+    <div className="flex items-center justify-center mt-28">
+      <div className="w-96 border rounded bg-yellow-500 px-7 py-10">
+        <form onSubmit={handleLogin}>
           <h4 className="text-2xl mb-7">Login</h4>
           <input 
             type="text" 
@@ -16,8 +38,22 @@ const Login = () => {
             className="input-box" 
             value={email} 
             onChange={(e) => setEmail(e.target.value)} 
+
           />
-          <PasswordInput />
+          <PasswordInput value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {error && <p className="text-red-700 text-sm pb-1">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            Login
+          </button>
+          <p className="text-sm text-center mt-4">
+            Not registred yet?{" "}
+            <Link to={"/signup"} className="font-medium text-[#2B8] underline">
+            Create an account</Link>
+          </p>
         </form>
       </div>
     </div>
