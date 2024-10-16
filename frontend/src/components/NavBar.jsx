f16136531fdd90410edb7e650548f56841ce4507
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-16 21:07:00 +0100

    frontend SearchBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SearchBar from './SerachBar/SearchBar'
+import ProfileInfo from './Cards/ProfileInfo'
 
 const NavBar = () => {
+    const [searchQuery, setSearchQuery] = useState("")
+
+    const handleSearch = () => {}
+    const onClearSearch = () => {
+      setSearchQuery("")
+    }
+    
   return (
-    <div className="bg-white">
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black">
         <span className="text-red-600">Burokkun</span>
         <span className="text-red-600">ōto</span>
       </h2>
+
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => setSearchQuery(target.value)}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch} />
+
+      <ProfileInfo />
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="bg-gray-500">Home</div>
+    <div className="bg-yellow-100">Home</div>
   )
 }
 
