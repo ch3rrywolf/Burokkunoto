99a98565880626da085ecaf8e4a282dd7aac7f39
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-16 19:37:53 +0100

    init frontend React,tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login/Login"
+import Home from "./pages/Home/Home"
+import Signup from "./pages/Signup/Signup"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
