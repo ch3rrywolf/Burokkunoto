89f03271dc63d16f50b2e3df64a93f9d08a5ff84
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-16 22:23:54 +0100

    frontend Login form

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PasswordInput from '../../components/Input/PasswordInput'
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
   return (
-    <div>Login</div>
+    <div className="">
+      <div>
+        <form>
+          <h4 className="text-2xl mb-7">Login</h4>
+          <input 
+            type="text" 
+            placeholder="Email" 
+            className="input-box" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <PasswordInput />
+        </form>
+      </div>
+    </div>
   )
 }
 
