1e26a1fdc85db1b229fd69fd7fa98526f32a9063
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-17 17:18:11 +0100

    backend start server && connect database

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
+mongoose.connect(
+    process.env.MONGO_URI
+).then(() => {
+    console.log("Connected to mongoDB")
+}).catch((err) => {
+    console.log(err)
+})
+
 const app = express()
 
 app.listen(3000, () => {
