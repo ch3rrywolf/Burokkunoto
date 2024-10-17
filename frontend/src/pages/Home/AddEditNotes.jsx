d40028492a751b66f6da5f727564c6e341a03b2d
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-17 14:09:52 +0100

    frontend Home  Modal{open}

--- frontend/src/pages/Home/AddEditNotes.jsx
+++ frontend/src/pages/Home/AddEditNotes.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdClose } from 'react-icons/md'
 
-const AddEditNotes = () => {
+const AddEditNotes = ({ onClose }) => {
+    const [title, setTitle] = useState("")
   return (
-    <div>AddEditNotes</div>
+    <div className="relative">
+        <button
+        className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3
+        -right-3 hover:bg-slate-50"
+        onClick={onClose}
+        >
+            <MdClose className="text-xl text-slate-400" />
+        </button>
+        <div>
+            <label className="input-label text-red-400 uppercase">Title</label>
+
+            <input
+                type="text"
+                className="text-2xl text-slate-950 outline-none"
+                placeholder="test test"
+                value={title}
+                onChange={({ target }) => setTitle(target.value)}
+             />
+        </div>
+
+    </div>
   )
 }
 
