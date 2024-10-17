7fb019f26e4063c0891a9ba2d2be2406b41b8d49
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-17 18:14:35 +0100

    backend controllers auth.controller {signup}

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,11 +1,32 @@
 import User from "../models/user.model";
+import { errorHandler } from "../utils/error";
+import bcryptjs from "bcryptjs"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body
 
     const isValidUser = await User.findOne({email})
 
     if(isValidUser) {
-        console.log("User already exist");
+        return next(errorHandler(400, "User already exist"));
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    try {
+        await newUser.save()
+
+        res.status(201).json({
+            success: true,
+            message: "User Created Successfully",
+        })
+    } catch (error) {
+        next(error)
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import  express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
@@ -14,6 +15,25 @@ mongoose.connect(
 
 const app = express()
 
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: "*" }))
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
+})
+
+import authRouter from "./routes/auth.route.js"
+
+app.use("/api/auth", authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
 })
