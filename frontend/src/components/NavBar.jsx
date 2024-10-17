eace921cc4be75d89be5933bddcff9580ecc4a96
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-17 15:26:28 +0100

    frontend.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import SearchBar from './SerachBar/SearchBar'
 import ProfileInfo from './Cards/ProfileInfo'
 import { useNavigate } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     const [searchQuery, setSearchQuery] = useState("")
@@ -20,10 +21,12 @@ const NavBar = () => {
     
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <Link to={"/"}>
       <h2 className="text-xl font-medium text-black">
         <span className="text-red-600 font-bold ">Burokkun</span>
         <span className="text-red-600">ōto</span>
       </h2>
+      </Link>
 
       <SearchBar
         value={searchQuery}
