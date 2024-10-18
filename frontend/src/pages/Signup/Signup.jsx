9ca6081b820002bdaaa5ea592ca4141839feb924
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-10-18 23:51:35 +0100

    frontend&backend sign up style

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -63,21 +63,21 @@ const Signup = () => {
 
 
   return (
-    <div className="flex items-center justify-center mt-28">
-      <div className="w-96 border rounded-full bg-red-600 px-7 py-10 ">
-        <form onSubmit={handleSignUp}>
-          <h4 className="text-2xl mb-7">Sign Up</h4>
+    <div className="flex items-center justify-center  h-screen">
+      <div className="w-96 h-96 rounded-full bg-red-600 flex items-center justify-center p-7 ">
+        <form onSubmit={handleSignUp} className="w-full text-center">
+          <h4 className="text-2xl mb-7 text-black">サインアップ</h4>
           <input 
             type="text" 
-            placeholder="Name" 
-            className="input-box" 
+            placeholder="名前" 
+            className="w-full bg-red-600 mb-4 px-4 py-2 text-sm text-white rounded-lg focus:outline-none" 
             value={name} 
             onChange={(e) => setName(e.target.value)}
           />
           <input 
             type="text" 
-            placeholder="Email" 
-            className="input-box" 
+            placeholder="電子メール" 
+            className="w-full bg-red-600 mb-4 px-4 py-2 text-sm text-white rounded-lg focus:outline-none" 
             value={email} 
             onChange={(e) => setEmail(e.target.value)} 
 
@@ -88,13 +88,13 @@ const Signup = () => {
 
           {error && <p className="text-red-700 text-sm pb-1">{error}</p>}
 
-          <button type="submit" className="btn-primary">
-            Sign Up
+          <button type="submit" className="w-full bg-red-600 text-red-600 font-semibold py-2 rounded-lg hover:bg-white focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50">
+            サインアップ
           </button>
-          <p className="text-sm text-center mt-4">
-            Already have an account?{" "}
+          <p className="text-sm mt-4 text-white">
+          すでにアカウントをお持ちですか?{" "}<br></br>
             <Link to={"/login"} className="font-medium text-[#2B8] underline">
-            login ur account</Link>
+            アカウントにログインしてください</Link>
           </p>
         </form>
       </div>
